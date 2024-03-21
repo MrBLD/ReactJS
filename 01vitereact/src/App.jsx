b919e4a3924b068a8f424a350f079140fc459245
MrBLD <bhaskarlda@gmail.com>
2024-03-22 01:56:16 +0530

    vitereact

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,14 @@
+import Chai from "./Chai.jsx"
+
+
 function App() {
   return (
-    <h1>Chai aur code on vite</h1>
+    // <h1>Chai aur code on vite</h1>
+    <>
+      <Chai/>
+      <h1> chai aur react</h1>
+      <p>test para</p>
+    </>
   )
 }
 
