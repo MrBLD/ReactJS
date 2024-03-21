20759a01c4207d80693b483e5e533bd6163c81aa
MrBLD <bhaskarlda@gmail.com>
2024-03-22 03:00:27 +0530

    basicReact update

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,6 +1,12 @@
+
+import Chai from './chai.js';
+
 function App() {
   return (
-    <h1>chai aur react</h1>
+    <>
+    <Chai/>
+    {/* <h1>chai aur react</h1> */}
+    </>
   );
 }
 
---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,8 @@
+
+function Chai(){
+    return(
+        <h2>chai aur basicReact</h2>
+    )
+}
+
+export default Chai
