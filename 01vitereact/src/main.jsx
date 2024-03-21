db093791b31ccd68e43cb7177274c5424c49163c
MrBLD <bhaskarlda@gmail.com>
2024-03-22 04:16:45 +0530

    vite main jsx update

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,47 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    < div >
+      <h1>Custom App</h1>
+    </div >
+  )
+}
+
+// const ReactElement={
+//   type:'a',
+//   props:{
+//       href: "https:/google.com",
+//       target:'_blank'
+//   },
+//   children:'Click me to visit Google'
+// }
+
+const anotherElement=(
+<a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "chai aur react"
+
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com',target: '_blank' },
+  'click me to visit google',
+  anotherElement
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <App />
+  // <ReactElement/> //reactelement is a js not a jsx(js object with html tag to parse into js) not a prerendered js object
+  // ReactElement // not work as render(react) method requires a specific input syntax
+  
+  reactElement
+  // <MyApp />
+  // MyApp()
+  // anotherElement
+  // <App />
   // </React.StrictMode>,
 )
