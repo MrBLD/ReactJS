9f6c346f0109a90860660a8c97537dce91b0d7a9
MrBLD <bhaskarlda@gmail.com>
2024-03-24 02:26:07 +0530

    04_tailwind props update

---
+++ 04_tailwindProps/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card  from '../Components/Card'
+
+function App() {
+
+  let obj={
+    name: "React",
+    age: 21
+  }
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>TailWind Test</h1>
+      <Card username="Lucy" btntext='Press Me'/>
+      <Card username="Lia" btntext='Touch Me'/>
+    </>
+  )
+}
+
+export default App
